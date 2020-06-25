ddeb05fb155425f6497d3a64484bc947e6464626
tenebrius1 <57551677+tenebrius1@users.noreply.github.com>
2020-06-25 22:58:56 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, redirect
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/")
+def index():
+    return render_template("index.html")
