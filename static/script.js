ba1c584d825e607a55812a5fdd49d36fb69ca3dc
tenebrius1 <57551677+tenebrius1@users.noreply.github.com>
2020-06-26 19:17:46 +0800

    updated landing page

---
+++ static/script.js
@@ -0,0 +1,7 @@
+window.onscroll = function() {minimizeNavBar()};
+
+function minimizeNavBar() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        
+    }
+}
