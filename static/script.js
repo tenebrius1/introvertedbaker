f5cfab6c6babda2ba8f04b92ceda2c207acf69e5
tenebrius1 <57551677+tenebrius1@users.noreply.github.com>
2020-06-26 21:20:36 +0800

    added responsive navbar shrinking

--- static/script.js
+++ static/script.js
@@ -1,7 +1,16 @@
 window.onscroll = function() {minimizeNavBar()};
 
 function minimizeNavBar() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        
+    if (document.body.scrollTop >= 80 || document.documentElement.scrollTop >= 80) {
+        console.log("here")
+        document.getElementById("navbarSupportedContent").classList.add("justify-content-end")
+        document.getElementById("navbar_container").style.flexDirection = "row"
+        document.getElementById("logo").style.height = "50px"
+        document.getElementById("logo").style.width = "100px"
+    } else {
+        document.getElementById("navbarSupportedContent").classList.remove("justify-content-end")
+        document.getElementById("navbar_container").style.flexDirection = "column"
+        document.getElementById("logo").style.height = "100px"
+        document.getElementById("logo").style.width = "200px"
     }
 }
