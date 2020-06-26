c0d256c8dd66a67aac9e9f68e4c7746486f2d29d
tenebrius1 <57551677+tenebrius1@users.noreply.github.com>
2020-06-26 22:22:07 +0800

    updated shrinking navbar to accomodate phone

--- static/script.js
+++ static/script.js
@@ -1,16 +1,28 @@
-window.onscroll = function() {minimizeNavBar()};
+window.onload = function () {
+  minimizeNavBar();
+};
+window.onscroll = function () {
+  minimizeNavBar();
+};
 
 function minimizeNavBar() {
-    if (document.body.scrollTop >= 80 || document.documentElement.scrollTop >= 80) {
-        console.log("here")
-        document.getElementById("navbarSupportedContent").classList.add("justify-content-end")
-        document.getElementById("navbar_container").style.flexDirection = "row"
-        document.getElementById("logo").style.height = "50px"
-        document.getElementById("logo").style.width = "100px"
-    } else {
-        document.getElementById("navbarSupportedContent").classList.remove("justify-content-end")
-        document.getElementById("navbar_container").style.flexDirection = "column"
-        document.getElementById("logo").style.height = "100px"
-        document.getElementById("logo").style.width = "200px"
-    }
+  if (
+    document.body.scrollTop >= 70 ||
+    document.documentElement.scrollTop >= 70 &&
+    window.matchMedia("(min-width: 480px)").matches
+  ) {
+    document
+      .getElementById("navbarSupportedContent")
+      .classList.add("justify-content-end");
+    document.getElementById("navbar_container").style.flexDirection = "row";
+    document.getElementById("logo").style.height = "50px";
+    document.getElementById("logo").style.width = "100px";
+  } else {
+    document
+      .getElementById("navbarSupportedContent")
+      .classList.remove("justify-content-end");
+    document.getElementById("navbar_container").style.flexDirection = "column";
+    document.getElementById("logo").style.height = "100px";
+    document.getElementById("logo").style.width = "200px";
+  }
 }
